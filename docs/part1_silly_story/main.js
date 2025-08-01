c315dab0e13b8ae7c7be8d2c582a45d730d2428f
Mirrage1009 <mirragerhmn@gmail.com>
2025-08-01 18:52:25 -0400

    c2

--- docs/part1_silly_story/main.js
+++ docs/part1_silly_story/main.js
@@ -1,3 +1,30 @@
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if (document.querySelector('input[value="uk"]').checked) {
+    const weight = Math.round(300 * 0.0714286) + ' stone';
+    const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
